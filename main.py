643379258ba1fa1531bdc1a2ddd737a6528338ac
iaroslavm <iaroslav.miller@gmail.com>
2023-06-01 12:25:05 -0400

    commit 4: adding methods to generate data features

--- main.py
+++ main.py
@@ -19,6 +19,28 @@ from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 
 
+# create timestamp udfs
+get_hour = udf(lambda x: dt.datetime.fromtimestamp(x / 1000.0).hour)
+get_year = udf(lambda x: dt.datetime.fromtimestamp(x / 1000.0).year)
+get_month = udf(lambda x: dt.datetime.fromtimestamp(x / 1000.0).month)
+get_day = udf(lambda x: dt.datetime.fromtimestamp(x / 1000.0).day)
+# create data features udfs
+check_for_churn = udf(lambda x: 1 if x == 'Cancellation Confirmation' else 0, IntegerType())
+check_for_downgrade_visit = udf(lambda x: 1 if x == 'Downgrade' else 0, IntegerType())
+check_for_downgrade_submit = udf(lambda x: 1 if x == 'Submit Downgrade' else 0, IntegerType())
+check_for_playlist = udf(lambda x: 1 if x == 'Add to Playlist' else 0, IntegerType())
+check_for_friends = udf(lambda x: 1 if x == 'Add Friend' else 0, IntegerType())
+check_for_adverts = udf(lambda x: 1 if x == 'Roll Advert' else 0, IntegerType())
+check_for_logout = udf(lambda x: 1 if x == 'Logout' else 0, IntegerType())
+song_event = udf(lambda x: 1 if x == 'NextSong' else 0, IntegerType())
+check_level = udf(lambda x: 1 if x == 'paid' else 0, IntegerType())
+check_thumbsup = udf(lambda x: 1 if x == 'Thumbs Up' else 0, IntegerType())
+check_thumbsdown = udf(lambda x: 1 if x == 'Thumbs Down' else 0, IntegerType())
+
+# create window for partition
+windowsum = Window.partitionBy("userId")
+
+
 def get_new_spark_session():
     # create a Spark session
     spark = SparkSession \
@@ -48,6 +70,44 @@ def clean_data(user_log):
                                      'song', 'artist', 'gender', 'itemInSession', \
                                      'length', 'level', 'location', 'page', 'status',\
                                      'ts', 'userAgent'])
+
+    return selected_data
+
+
+def create_time_columns(selected_data):
+    selected_data = selected_data.withColumn('year', get_year(selected_data.ts)) \
+        .withColumn('month', get_month(selected_data.ts)) \
+        .withColumn('day', get_day(selected_data.ts)) \
+        .withColumn('hour', get_hour(selected_data.ts))
+
+    # calculate start and end time for each user in the database
+    selected_data = selected_data\
+        .withColumn('endDate', from_unixtime(maximum(selected_data.ts / 1000.0).over(windowsum))) \
+        .withColumn('startDate', from_unixtime(minimum(selected_data.ts / 1000.0).over(windowsum)))
+
+    # calculate duration per user
+    selected_data = selected_data \
+        .withColumn('durationDays', datediff('endDate', 'startDate'))
+
+    # adding weekly timestamps to track behaviour over time
+    selected_data = selected_data.withColumn('weekBeforeEndDate', date_add(selected_data.endDate, -7)) \
+        .withColumn('twoweekBeforeEndDate', date_add(selected_data.endDate, -14)) \
+        .withColumn('threeweekBeforeEndDate', date_add(selected_data.endDate, -21)) \
+        .withColumn('fourweekBeforeEndDate', date_add(selected_data.endDate, -28))
+
+    return selected_data
+
+def create_user_features(selected_data):
+    selected_data = selected_data.withColumn('churn', check_for_churn('page')) \
+        .withColumn('downgradeVisit', check_for_downgrade_visit('page')) \
+        .withColumn('downgradeSubmit', check_for_downgrade_submit('page')) \
+        .withColumn('addToPlaylist', check_for_playlist('page')) \
+        .withColumn('addFriend', check_for_friends('page')) \
+        .withColumn('rollAdvert', check_for_adverts('page')) \
+        .withColumn('logout', check_for_logout('page')) \
+        .withColumn('paingUser', check_for_logout('level')) \
+        .withColumn('Up', check_thumbsup('page')) \
+        .withColumn('Down', check_thumbsdown('page'))
 
     return selected_data
 
